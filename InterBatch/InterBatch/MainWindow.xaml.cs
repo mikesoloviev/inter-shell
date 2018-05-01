9011a9186a3e6d92e165fc49ac09a22b39e4b297
mikesoloviev <38215251+mikesoloviev@users.noreply.github.com>
2018-05-01 08:54:22 -0400

    Add files via upload

--- InterBatch/InterBatch/MainWindow.xaml.cs
+++ InterBatch/InterBatch/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace InterBatch {
+namespace Viscont {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
