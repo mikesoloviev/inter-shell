12694460b95113cfd86750a798a8ecf44aea16c1
mikesoloviev <38215251+mikesoloviev@users.noreply.github.com>
2018-05-04 17:08:55 -0400

    v

--- InterShell/InterShell/Components/MarkLite.cs
+++ InterShell/InterShell/Components/MarkLite.cs
@@ -22,6 +22,7 @@ namespace InterShell.Components {
             text.Append(Head);
             foreach (var line in lines) {
                 text.AppendLine(MarkLine(line));
+                prevLine = line.Trim();
             }
             text.Append(Tail);
             return text.ToString();
@@ -33,10 +34,14 @@ namespace InterShell.Components {
 
         bool listBlock = false;
         bool codeBlock = false;
+        bool paraBlock = false;
+        string prevLine = "";
 
         void Reset() {
             listBlock = false;
             codeBlock = false;
+            paraBlock = false;
+            prevLine = "";
         }
 
         string MarkLine(string line) {
@@ -46,6 +51,7 @@ namespace InterShell.Components {
         string MarkBlock(string line) {
             var result = new StringBuilder();
             if (line.StartsWith("* ")) {
+                paraBlock = false;
                 if (!listBlock) {
                     listBlock = true;
                     result.AppendLine("<ul>");
@@ -58,31 +64,47 @@ namespace InterShell.Components {
                     result.AppendLine("</ul>");
                 }
                 if (line.StartsWith("***")) {
+                    paraBlock = false;
                     result.AppendLine("<hr/>");
                 }
                 else if (line.StartsWith("```")) {
+                    paraBlock = false;
                     if (codeBlock) {
                         codeBlock = false;
-                        result.AppendLine("</code>");
+                        result.AppendLine("</pre>");
                     }
                     else {
                         codeBlock = true;
-                        result.AppendLine("<code>");
+                        result.AppendLine("<pre>");
                     }
                 }
                 else if (line.StartsWith("# ")) {
+                    paraBlock = false;
                     result.AppendLine(Enclose(line, '#', "h1"));
                 }
                 else if (line.StartsWith("## ")) {
+                    paraBlock = false;
                     result.AppendLine(Enclose(line, '#', "h2"));
                 }
                 else if (line.StartsWith("### ")) {
+                    paraBlock = false;
                     result.AppendLine(Enclose(line, '#', "h3"));
                 }
                 else if (line.StartsWith("#### ")) {
+                    paraBlock = false;
                     result.AppendLine(Enclose(line, '#', "h4"));
                 }
                 else {
+                    if (!codeBlock) {
+                        if (prevLine == "") {
+                            if (paraBlock) {
+
+                            }
+                            else {
+
+                            }
+                        }
+                    }
                     result.AppendLine(line);
                 }
             }
@@ -118,11 +140,76 @@ namespace InterShell.Components {
 
         #region Static Content
 
-        string Head = 
+        string Head =
 @"
 <html>
 <head>
 <style>
+body {
+  font-family: 'Segoe UI', Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.5;
+  background-color: white;
+  color: #202020;
+  margin-top: 10px;
+  margin-left: 10px;
+}
+h1, h2, h3, h4, h5, h6 {
+  margin: 20px 0 10px;
+  padding: 0;
+  font-weight: bold;
+  cursor: text;
+  position: relative;
+}
+h1 {
+  font-size: 24px;
+  border-bottom: 1px solid #C0C0C0;
+}
+h2 {
+  font-size: 20px;
+  border-bottom: 1px solid #C0C0C0;
+}
+h3 {
+  font-size: 18px;
+  border-bottom: 1px solid #C0C0C0;
+}
+h4 {
+  font-size: 16px;
+}
+hr {
+  border: 0 none;
+  color: #C0C0C0;
+  height: 1px;
+  padding: 0;
+}
+ul {
+  margin-left: 15px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+b {
+  color: #202040;
+}
+i {
+  color: #202060;
+}
+code {
+  color: #000020;
+  background-color: #F0F0F0;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+pre {
+  color: #000020;
+  background-color: #F0F0F0;
+  line-height: 0.75;
+  padding: 0px 10px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #E0E0E0;
+  border-radius: 4px;
+  overflow: auto;
+}
 </style>
 </head>
 <body>
