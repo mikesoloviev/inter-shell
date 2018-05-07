55f894f325975a726bc078bfead04ffb33f03011
mikesoloviev <38215251+mikesoloviev@users.noreply.github.com>
2018-05-07 12:20:13 -0400

    v 05-07

---
+++ InterShell/Common.cs
@@ -0,0 +1,28 @@
+﻿namespace InterShell {
+
+    public class Config { 
+
+        public static string AppName = "InterShell";
+        public static string Home = ".";
+
+        public static string PrefFile = $"{AppName}.ini";
+        public static string DataFile = $"{AppName}.dat";
+        public static string HelpFile = $"{AppName}.md";
+        public static string ShellFile = $"{AppName}.bat";
+        public static string OutFile = $"{AppName}.out";
+
+        public static int WindowWidth = 800;
+        public static int WindowHeight = 600;
+    }
+
+    public class Selection { 
+        public const int None = -1;
+    }
+
+    public enum Division { 
+        Library,
+        Group, 
+        Command,
+        Setting
+    }
+}
