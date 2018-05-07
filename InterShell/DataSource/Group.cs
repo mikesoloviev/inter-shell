9336a816ca28b0f527d1f7ffeaaec4fbfc97406c
mikesoloviev <38215251+mikesoloviev@users.noreply.github.com>
2018-05-07 12:22:10 -0400

    v 05-07

--- InterShell/DataSource/Group.cs
+++ InterShell/DataSource/Group.cs
@@ -1,1 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace InterShell.DataSource {
+
+    public class Group {
+
+        public string Name { get; set; } = "";
+        public List<string> Notes { get; set; } = new List<string>();
+        public List<Setting> Settings { get; set; } = new List<Setting>();
+        public List<Command> Commands { get; set; } = new List<Command>();
+
+        public string Note { get { return Notes.First() ?? ""; } }
+        public bool IsEmpty { get { return Name.Length == 0; } }
+
+        public Dictionary<string, string> GetSettingSet() {
+            var settingSet = new Dictionary<string, string>();
+            foreach (var setting in Settings) {
+                settingSet[setting.Name] = setting.Value;
+            }
+            return settingSet;
+        }
+
+        public List<string> Encode() {
+            if (IsEmpty) return new List<string>();
+            var code = new List<string> { $"# {Name}" };
+            code.AddRange(Notes.Select(x => $"- {x}"));
+            code.Add("");
+            foreach (var setting in Settings) {
+                code.AddRange(setting.Encode());
+            }
+            code.Add("");
+            foreach (var command in Commands) {
+                code.AddRange(command.Encode());
+                code.Add("");
+            }
+            return code;
+        }
+
+    }
+}
---
+++ InterShell/DataSource/Setting.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterShell.DataSource {
+
+    public class Setting {
+
+        public string Name { get; set; } = "";
+        public string Value { get; set; } = "";
+        public List<string> Options { get; set; } = new List<string>();
+
+        public bool IsEmpty { get { return Name.Length == 0; } }
+
+        public Setting() {
+        }
+
+        public Setting(string[] fields) {
+            Name = fields.Length > 0 ? fields[0].Trim() : "";
+            Value = fields.Length > 1 ? fields[1].Trim() : "";
+        }
+
+        public Setting Clone() {
+            var cc = new Setting();
+            cc.Name = Name;
+            cc.Value = Value;
+            cc.Options = Options;
+            return cc;
+        }
+
+        public void AddOptions(string[] options) {
+            foreach (var option in options) {
+                Options.Add(option.Trim());
+            }
+        }
+
+        public List<string> Encode() {
+            if (IsEmpty) return new List<string>();
+            var code = new List<string> { $"{Name} = {Value}" };
+            if (Options.Any()) code.Add($": {string.Join(", ", Options)}");
+            return code;
+        }
+
+    }
+}
